fc92d4256c844db49c095c8af18658b8867e8886
DavidBarta92 <blj.david@gmail.com>
2020-06-10 14:24:14 +0100

    empty list solved

--- src/todo/Argument.java
+++ src/todo/Argument.java
@@ -7,14 +7,17 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Argument {
-    private static File file;
-
     public static void list() throws IOException {
         try {
             Path filePath = Paths.get("todo-data.txt");
             List<String> lines = Files.readAllLines(filePath);
-            for (int i = 0; i< lines.size(); i++){
-                System.out.println((i+1) + " - " + lines.get(i));
+            if (lines.size() > 0){
+                for (int i = 0; i< lines.size(); i++){
+                    System.out.println((i+1) + " - " + lines.get(i));
+                }
+            }
+            else {
+                System.out.println("No todos for today! :)");
             }
         } catch (Exception e){
             System.out.print("Unable to read file: todo-data.txt");
